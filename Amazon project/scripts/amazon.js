089c7ef9491fa31ce6f5385d2210bdf1ef97514e
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-22 22:18:21 +0530

    cart quantity

--- Amazon project/scripts/amazon.js
+++ Amazon project/scripts/amazon.js
@@ -78,6 +78,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   
     });
   }
+
+  let cartQuantity=0;
+  cart.forEach((item)=>{
+    cartQuantity+=item.quantity;
+  });
+  console.log(cartQuantity);
   console.log(cart);
   });
 
