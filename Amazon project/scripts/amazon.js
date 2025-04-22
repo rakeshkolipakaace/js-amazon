02fa317c4f6ef82e601581bd250ac007d7def70b
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-04-22 22:07:04 +0530

    matching item

--- Amazon project/scripts/amazon.js
+++ Amazon project/scripts/amazon.js
@@ -62,6 +62,12 @@ document.querySelector('.js-pproducts-grid').innerHTML=productsHTML;
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',()=>{
   const productName=button.dataset.productName;
+  let matchingItem;
+  cart.forEach((item)=>{
+    if(productName===item.productName){
+      matchingItem=item;
+    }
+  })
   cart.push({
     productName: productName,
     quantity:1,
