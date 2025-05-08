806e9fefb862e38792cea0d43410288decbec88c
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-08 18:55:38 +0530

    moneyTest.js

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -5,3 +5,17 @@ if(formatCurrency(2095)==='20.95'){
 }else{
     console.log('Test failed');
 }
+
+
+if(formatCurrency(0)==='0.00'){
+    console.log('Test passed');
+}else{
+    console.log('Test failed');
+}
+
+
+if(formatCurrency(2000.5)==='20.01'){
+    console.log('Test passed'); 
+}else{
+    console.log('Test failed');
+}
