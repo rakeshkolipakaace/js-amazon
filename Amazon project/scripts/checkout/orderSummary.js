c2a500da492d1e5f2eba08f09a64ea902639be45
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-08 20:37:53 +0530

    order test

--- Amazon project/scripts/checkout/orderSummary.js
+++ Amazon project/scripts/checkout/orderSummary.js
@@ -29,7 +29,6 @@ export function renderOrderSummary() {
 
     cartSummaryHTML += `
       <div class="cart-item-container
-        js-cart-item-container
         js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: ${dateString}
