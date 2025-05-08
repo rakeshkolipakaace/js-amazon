8784a8e39ad4099c816a7a9b006fcb6077728aea
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-08 22:10:43 +0530

    orde1r qunatity delivery option

--- Amazon project/tests-jasmine/checkout/orderSummaryTest.js
+++ Amazon project/tests-jasmine/checkout/orderSummaryTest.js
@@ -7,10 +7,12 @@ describe('test suite : renderOrderSummary', () => {
     it('displays the cart ',()=>{
     document.querySelector('.js-test-container').innerHTML = `
     <div class="js-order-summary"></div>`;
+
+    const productId1='e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     
     spyOn(localStorage, 'getItem').and.callFake(() => {
         return JSON.stringify([{
-            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            productId: productId1,
             quantity: 2,
             deliveryOptionId: '1'
           }, {
@@ -25,5 +27,11 @@ loadFromStorage();
 renderOrderSummary();
 
 expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2);
+
+
+expect(
+    document.querySelector(`.js-product-quantity-${productId1}`).innerText
+  ).toContain('Quantity: 2');
+
     });
 });
