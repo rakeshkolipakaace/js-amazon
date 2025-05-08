53d8eb49d6675e40eecaa8f865d44ca315416efa
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-08 22:20:14 +0530

    delete product

--- Amazon project/tests-jasmine/checkout/orderSummaryTest.js
+++ Amazon project/tests-jasmine/checkout/orderSummaryTest.js
@@ -39,4 +39,35 @@ expect(
   ).toContain('Quantity: 1');
 
     });
+
+
+    it('removes a product ',()=>{
+        document.querySelector('.js-test-container').innerHTML = `
+    <div class="js-order-summary"></div>
+    <div class="js-payment-summary"></div>`;
+
+
+    const productId1='e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
+    const productId2='15b6fc6f-327a-4ec4-896f-486349e85a3d';
+    
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+        return JSON.stringify([{
+            productId: productId1,
+            quantity: 2,
+            deliveryOptionId: '1'
+          }, {
+            productId: productId2,
+            quantity: 1,
+            deliveryOptionId: '2'
+          }]);
+      });
+
+loadFromStorage();
+
+renderOrderSummary();
+
+
+document.querySelector(`.js-delete-link-${productId1}`).click();
+
+    })
 });
