d8f210eda06a87f8b1ee6a0a8cb8601e9b0054ca
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-09 11:29:23 +0530

    cart function

--- Amazon project/data/cart-oop.js
+++ Amazon project/data/cart-oop.js
@@ -1,4 +1,5 @@
-const cart={
+function Cart(){
+  const cart={
     cartItems:undefined,
 
    loadFromStorage() {
@@ -78,93 +79,17 @@ updateDeliveryOption(productId, deliveryOptionId) {
 
 };
 
+return cart;
 
-
-cart.loadFromStorage();
-console.log(cart)
-
-
-const businessCart={
-    cartItems:undefined,
-
-   loadFromStorage() {
-  this.cartItems = JSON.parse(localStorage.getItem('cart-business'));
-
-  if (!this.cartItems) {
-    this.cartItems = [{
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-      deliveryOptionId: '1'
-    }, {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1,
-      deliveryOptionId: '2'
-    }];
-  }
-},
-
-saveToStorage() {
-  localStorage.setItem('cart-business', JSON.stringify(this.cartItems));
-},
-
-
- addToCart(productId) {
-  let matchingItem;
-
- this.cartItems.forEach((cartItem) => {
-    if (productId === cartItem.productId) {
-      matchingItem = cartItem;
-    }
-  });
-
-  if (matchingItem) {
-    matchingItem.quantity += 1;
-  } else {
-    this.cartItems.push({
-      productId: productId,
-      quantity: 1,
-      deliveryOptionId: '1'
-    });
-  }
-
-  this.saveToStorage();
-},
-
-
-
-removeFromCart(productId) {
-  const newCart = [];
-
-  this.cartItems.forEach((cartItem) => {
-    if (cartItem.productId !== productId) {
-      newCart.push(cartItem);
-    }
-  });
-
-  this.cartItems = newCart;
-
-  this.saveToStorage();
-},
-
-updateDeliveryOption(productId, deliveryOptionId) {
-  let matchingItem;
-
-  this.cartItems.forEach((cartItem) => {
-    if (productId === cartItem.productId) {
-      matchingItem = cartItem;
-    }
-  });
-
-  matchingItem.deliveryOptionId = deliveryOptionId;
-
-  this.saveToStorage();
 }
 
 
 
-};
+const cart=Cart();
+const businessCart= Cart();
 
-
+cart.loadFromStorage();
+console.log(cart);
 
 businessCart.loadFromStorage();
 
