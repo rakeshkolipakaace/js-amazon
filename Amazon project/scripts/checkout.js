d818321ad967a90b099b96136793706c15519a0d
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-09 11:11:17 +0530

    checkout.js

--- Amazon project/scripts/checkout.js
+++ Amazon project/scripts/checkout.js
@@ -207,6 +207,8 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
 import {renderOrderSummary} from './checkout/orderSummary.js';
 
+import '../data/cart-oop.js';
+
 
 renderOrderSummary();
 renderPaymentSummary();
