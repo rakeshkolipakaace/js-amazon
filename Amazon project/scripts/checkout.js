31e9fd72887ea61d36d6fb9304e561aae08df9dc
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-09 19:20:20 +0530

    import backend

--- Amazon project/scripts/checkout.js
+++ Amazon project/scripts/checkout.js
@@ -209,6 +209,8 @@ import {renderOrderSummary} from './checkout/orderSummary.js';
 
 // import '../data/cart-class.js';
 
+import '../data/backend-practice.js';
+
 
 renderOrderSummary();
 renderPaymentSummary();
