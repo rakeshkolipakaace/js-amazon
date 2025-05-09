840af3bc2ec839ee8d2560cda6d284f875de6ca3
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-09 20:06:23 +0530

    final scripts/checkout/paymentSummary.js

--- Amazon project/scripts/checkout/paymentSummary.js
+++ Amazon project/scripts/checkout/paymentSummary.js
@@ -66,4 +66,30 @@ export function renderPaymentSummary() {
 
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
+
+
+  
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+
+        const order = await response.json();
+        addOrder(order);
+
+      } catch (error) {
+        console.log('Unexpected error. Try again later.');
+      }
+
+      window.location.href = 'orders.html';
+    });
+
 }
