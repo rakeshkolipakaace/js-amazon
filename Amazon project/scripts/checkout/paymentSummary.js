ef8249158dc4dee8aa94e9cec85de00b7a3b27f3
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-05-19 00:15:13 +0530

    paymentSummary.js

--- Amazon project/scripts/checkout/paymentSummary.js
+++ Amazon project/scripts/checkout/paymentSummary.js
@@ -2,6 +2,8 @@ import {cart} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
+import {addOrder} from '../../data/orders.js';
+
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
